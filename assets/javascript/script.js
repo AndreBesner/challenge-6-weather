35a613e66659830b5b905da049ad09554d6cda10
Andre Besner <besner.andre@gmail.com>
2023-04-07 18:20:07 -0400

    get user input fully working with data printing back

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
+
+
+    let previousSearches = [] ;
+
     $("#city-name-input").submit(function(event){
         event.preventDefault();
-        let cityName = $("#city-name").val();
+        let cityName = $("#city-name").val().trim(); // what user entered
         console.log(cityName);
+        printName(cityName);
+        printData(event);
+        previousSearches.push(cityName); // adds user search to array, to be saved to local storage
+        console.log(previousSearches);
+        localStorage.setItem(city, cityName);
     })
+
+    function printName(name){
+        console.log(name);
+    }
+    
+    function printData(data){
+        console.log(data);
+    }
+
 })
