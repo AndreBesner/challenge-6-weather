9bfc818e17a8ea0f66a815761ba7debd51433f93
Andre Besner <besner.andre@gmail.com>
2023-04-13 16:11:10 -0400

    Get the list of previous searchs actually working properly and in order

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,13 +3,28 @@ $(document).ready(function(){
     // Print the  5 searches as list items
     let previousSearchContainer = $("#previous-search-container");
     function printLastSearches(){
-        for(let i = localStorage.length - 1 ; i >= localStorage.length - 6 ; i -- ){
-            let key = localStorage.key(i);
-            // console.log(localStorage.getItem(key));
+        previousSearchContainer.empty();
+        // for(let i = localStorage.length - 1 ; i >= localStorage.length - 6 ; i -- ){
+        //     let key = localStorage.key(i);
+        //     // console.log(localStorage.getItem(key));
+        //     let makeListItem = document.createElement('li');
+        //     makeListItem.innerHTML = localStorage.getItem(key);
+        //     previousSearchContainer.append(makeListItem);
+        //     //make a link
+
+            //on click have it run function like below 
+            // makeListItem.addEventListener('click', (e)=>{
+            //     e.preventDefault(); // stops page from refreshing
+            //     let city = e.target.innerHTML;
+            //     getLongLat(city);
+            // })
+        // }
+
+        let cityArray = JSON.parse(localStorage.getItem("cityArray")) || [] ;
+        for(let i = cityArray.length - 1 ; i >= 0 ; i--){
             let makeListItem = document.createElement('li');
-            makeListItem.innerHTML = localStorage.getItem(key);
+            makeListItem.innerHTML = cityArray[i];
             previousSearchContainer.append(makeListItem);
-            //make a link
 
             //on click have it run function like below 
             makeListItem.addEventListener('click', (e)=>{
@@ -18,17 +33,34 @@ $(document).ready(function(){
                 getLongLat(city);
             })
         }
+
     }
 
-    printLastSearches();
+    // printLastSearches();
 
     let cityName = $("#city-name");
     $("#city-name-input").submit(function (e) { 
         e.preventDefault(); // stops page from refreshing
         let city = $("#city-name").val().trim(); // saves entered name to variable
         console.log(city)
-        let storageKey = "userText.." + Date.now(); // sets a key for individual local storage
-        localStorage.setItem(storageKey, city); // sets each search term to local storage
+
+        let cityArray = JSON.parse(localStorage.getItem("cityArray")) || [] ;
+
+        cityArray.push(city);
+
+        if(cityArray.length > 5){
+            cityArray.shift();
+        }
+
+        localStorage.setItem("cityArray", JSON.stringify(cityArray));
+
+        printLastSearches();
+
+        
+        // let storageKey = "userText.." + Date.now(); // sets a key for individual local storage
+        // localStorage.setItem(storageKey, city); // sets each search term to local storage
+
+
         getLongLat(city); // passes city text on to api call idek how but it works with gibberish
         cityName.val(""); // clears text in box
         //
