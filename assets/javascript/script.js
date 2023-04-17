f031bb8827d96d387d49dc8e67a3835ea4686f38
DreB <besner.andre@gmail.com>
2023-04-16 20:44:19 -0400

    add comments, clean up code, fix indentation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -126,7 +126,7 @@ $(document).ready(function(){
         // with the relevant weather information
         // it took me ages to figure out how to do this and implement bootstrap.
         for(let i = 7 ; i <= 40 ; i+=7){
-            let makeDiv = $("<div>").addClass('col bg-primary text-white border-black');
+            let makeDiv = $("<div>").addClass('col bg-primary text-white border');
             let makeList = document.createElement("ul");
             let makeIcon = document.createElement("img");
             let makeDate = document.createElement("li");
@@ -136,7 +136,8 @@ $(document).ready(function(){
             let iconCode = data.list[i].weather[0].icon;
             let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
             makeIcon.setAttribute('src', iconURL);
-            dateText = data.list[i].dt_txt
+            makeIcon.setAttribute('alt', 'icon to represent the weather');
+            dateText = data.list[i].dt_txt;
             makeDate.textContent = "Weather on: " + dateText.substring(0, 10);
             makeTemp.textContent = "Temp: " + data.list[i].main.temp + " Celcius";
             makeHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
